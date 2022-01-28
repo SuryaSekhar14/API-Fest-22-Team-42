75bf672d2728376f2aae1f39ae5c763ded056edd
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2022-01-28 19:58:27 +0530

    ?

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('api/get/', csrf_exempt(views.getData), name='getData'),
-    path('api/ocr/', csrf_exempt(views.ocr_api), name='ocr_api'),
+    path('getdata/', csrf_exempt(views.getData), name='getData'),
+    path('ocr/', views.ocr_api, name='ocr_api'),
 ]
