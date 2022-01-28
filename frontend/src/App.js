c8d6512efb648f8fb8b82dad2a2823329087dcaf
itisdb <dhriteshbhagat@gmail.com>
2022-01-29 01:25:02 +0530

    URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route path='/team' element={<Team />} />
         <Route path='/apis' element={<Apis />} />
         <Route path='/apis' element={<Apis />} />
-        <Route path='/APIpages/Ocr' element={<Ocr />} />
+        <Route path='/apipage/ccr' element={<Ocr />} />
         <Route path="*" element={<Error/>} />
       </Routes>
     </div>
