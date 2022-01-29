2e58734566e93b171d201a54523b79dcb6d8932f
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2022-01-29 11:51:15 +0530

    backend branch

--- frontend/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export {default as Navbar} from './Navbar';
-export {default as Hero} from './Hero';
-export {default as Footer} from './Footer';
-export {default as Team} from './Team';
-export {default as Stats} from './Stats';
-export {default as Apicards} from './Apicards';
-export {default as OcrComp} from './OCR/OcrComp';
-export {default as Translate} from './Translate/Translate';
--- frontend/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export {default as MainLayout} from './MainLayout';
-export {default as Error} from './404';
-export {default as Team} from './Team';
-export {default as Apis} from './Apis';
-export {default as Ocr} from './apipages/Ocr';
-export {default as Trans} from './apipages/Trans';
