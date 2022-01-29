29fe98a4205e4c50fd5d96932d03d3bd1022693a
itisdb <dhriteshbhagat@gmail.com>
2022-01-29 15:39:18 +0530

    update call

--- frontend/src/components/PDF/PdfCombiner.jsx
+++ frontend/src/components/PDF/PdfCombiner.jsx
@@ -4,7 +4,7 @@ import Dropzone from 'react-dropzone-uploader'
 import { getDroppedOrSelectedFiles } from 'html5-file-selector'
 import validator from 'validator'
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 // import fs from 'fs'
 
 const logo = require('../../assets/img/pdf.jpg');
@@ -45,7 +45,7 @@ const PdfCombiner = () => {
 
     const [showModal, setShowModal] = useState(false);
     const [emailError, setEmailError] = useState('')
-    const email = useState('')
+    const eid = useState('')
     const validateEmail = (e) => {
       var email = e.target.value
     
@@ -70,12 +70,13 @@ const PdfCombiner = () => {
             console.log(emailDone)
             // console.log(files)
             console.log(allFiles)
+            pdfapi(files, 'dhriteshbhagat@gmail.com')
         } else {
             emailDone = false;
             setShowModal(true)
             console.log(emailDone)
             // console.log(files)
-            console.log(allFiles)
+            // console.log(allFiles)
         }
             // pdfapi()
             // axios.post('http://
@@ -83,6 +84,37 @@ const PdfCombiner = () => {
         console.log(files.map(f => f.file))
         allFiles.forEach(f => f.remove())
         
+    }
+
+    const seteid = (e) => {
+        eid = e.target.value
+    }
+
+    const pdfapi = (files,eid) => {
+        var axios = require('axios');
+        var FormData = require('form-data');
+        // var fs = require('fs');
+        var data = new FormData();
+        files.map(f => data.append('image', f.file))
+        data.append('email', eid);
+
+        var config = {
+        method: 'post',
+        url: 'https://sads-apis-backend.herokuapp.com/api/imagetopdf/',
+        headers: { 
+        },
+        data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+        console.log(error);
+    });
+
+
     }
 
     const getFilesFromEvent = e => {
@@ -180,16 +212,6 @@ const PdfCombiner = () => {
                                     <img className="w-full h-full overflow-hidden object-cover rounded-full" src={logo} alt="avatar" />
                                 </div>
                                 <h2 className="text-indigo-800 text-2xl tracking-normal font-semibold mb-1">SADS PDF</h2>
-                                {/* <p className="flex text-gray-600 text-sm tracking-normal font-normal mb-3 text-center">
-                                    <span className="cursor-pointer mr-1 text-gray-600 dark:text-gray-100">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" />
-                                            <circle cx={12} cy={11} r={3} />
-                                            <path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 0 1 -2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0z" />
-                                        </svg>
-                                    </span>
-                                    Las Vegas, Nevada
-                                </p> */}
                                 <p className="text-gray-600 text-sm tracking-normal font-normal mb-8 text-left w-10/12">Here is the PDFs API, Select the Images and press submit once it is compiled you will recieve an output in your mail.
                                 <br/><br/>How to convert a PDF to JPG online:
                                 <br/>
@@ -202,46 +224,8 @@ const PdfCombiner = () => {
                                 [*] Enter the email address you want to send the PDF to.
                                 <br/>
                                 [*] Click Submit.</p>
-                                {/* <div className="flex items-start">
-                                    <div className>
-                                        <h2 className="text-gray-600 text-2xl leading-6 mb-2 text-center">82</h2>
-                                        <p className="text-gray-800 text-sm leading-5">Reviews</p>
-                                    </div>
-                                    <div className="mx-6 lg:mx-3 xl:mx-6 px-8 lg:px-4 xl:px-8 border-l border-r">
-                                        <h2 className="text-gray-600 text-2xl leading-6 mb-2 text-center">28</h2>
-                                        <p className="text-gray-800 text-sm leading-5">Projects</p>
-                                    </div>
-                                    <div className>
-                                        <h2 className="text-gray-600 text-2xl leading-6 mb-2 text-center">42</h2>
-                                        <p className="text-gray-800 text-sm leading-5">Approved</p>
-                                    </div>
-                                </div> */}
-                                {/* <select inputId="inputLang" className="block w-full text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" name="Input Language">
-                                <option value>
-                                    Input Language
-                                </option>
-                                {languages.map((lang) => (
-                                    <option className="text-gray-700" key={lang.id} value={lang.language}>{lang.language}</option>
-                                ))}
-                                </select>
-                                <select inputId="outputLang" className="mt-4 block w-full text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500" name="Input Language">
-                                <option value>
-                                    Output Language
-                                </option>
-                                {languages.map((lang) => (
-                                    <option className="text-gray-700" key={lang.id} value={lang.language}>{lang.language}</option>
-                                ))}
-                                </select> */}
-                                {/* {console.log(iLang.value)}
-                                {console.log(oLang.value)} */}
-                                    {/* <label htmlFor="email" className="text-gray-800 text-sm font-bold leading-tight tracking-normal mb-2">
-                                        Email
-                                    </label> */}
                                 <input id="email" className="text-gray-600 bg-white focus:outline-none focus:border focus:border-indigo-700 bg-white font-normal w-3/4 h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow" placeholder="Enter Email" onChange={(e) => validateEmail(e)}/>
                                 <span className="text-sm mt-1 text-gray-600 font-semibold">{emailError}</span>
-                                {/* <button type="button" className="py-2 px-4 mt-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                                Email me PDF ヾ(•ω•`)o
-                                </button> */}
                             </div>
                             <div className="w-full lg:w-1/2 px-6 mr-10 border-t border-b lg:border-t-0 border-gray-300 flex flex-col justify-center py-6">
                             <Dropzone
